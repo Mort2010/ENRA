187ce1543b8edfa4e3d08365c588b359aeddb7f6
Laptop <JamesC@mail.com>
2022-08-31 19:21:42 +0100

    Added login as requested (Normally would not commit user names or passwords)

---
+++ Resources/Secrets/oLogin.js
@@ -0,0 +1,10 @@
+const oGoodDetails = {username : 'tomsmith',
+password : 'SuperSecretPassword!'};
+
+const oBadDetails =  {username : 'tomsmit',
+password : 'SuperSecretPassword!'};
+
+module.exports ={
+    oGoodDetails, oBadDetails
+  };
+  
