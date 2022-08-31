c85d274d7624338ba492e5761c8c672d5d85a265
Laptop <JamesC@mail.com>
2022-08-31 19:28:02 +0100

    Fixed reporting bug

--- test/specs/login.js
+++ test/specs/login.js
@@ -12,15 +12,16 @@ describe('Login to Secure area', () => {
         await expect(SecurePage.flashAlert).toHaveTextContaining(
             'You logged into a secure area!');
     });
-});
 
-describe('Fail to login to Secure area', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-        await LoginPage.login(LoginCredentials.oBadDetails);
-
-        await expect(LoginPage.flash).toBeExisting();
-        await expect(LoginPage.flash).toHaveTextContaining(
-            'Your username is invalid!');
+    describe('Fail to login to Secure area', () => {
+        it('should login with valid credentials', async () => {
+            await LoginPage.open();
+            await LoginPage.login(LoginCredentials.oBadDetails);
+    
+            await expect(LoginPage.flash).toBeExisting();
+            await expect(LoginPage.flash).toHaveTextContaining(
+                'Your username is invalid!');
+        });
     });
 });
+
